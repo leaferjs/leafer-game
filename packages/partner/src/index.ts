325bc4b42815c6bbf7ecc022f5b402b5048c2899
leaferjs <leaferjs@163.com>
2024-11-04 21:51:16 +0800

    add @leafer-in/motion-path

--- packages/partner/src/index.ts
+++ packages/partner/src/index.ts
@@ -1,5 +1,6 @@
 export * from '@leafer-in/interface'
 
+export * from '@leafer-in/robot'
 export * from '@leafer-in/state'
 export * from '@leafer-in/animate'
-export * from '@leafer-in/robot'
+export * from '@leafer-in/motion-path'
